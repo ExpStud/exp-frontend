6d1a73eb1fd959812b391181f775f64ccb1d48c3
Darth Degen <darthdegen@gmail.com>
2023-07-19 16:24:33 -0700

    updated meta

--- src/components/templates/LandingView.tsx
+++ src/components/templates/LandingView.tsx
@@ -15,11 +15,16 @@ const LandingView: FC<Props> = (props: Props) => {
   return (
     <div className="relative w-full h-full flex flex-col items-center justify-center">
       <motion.div {...slideUp(showView)}>
-        <h2 className=" text-xl md:text-6xl px-5 text-center">
+        <h2 className=" text-2xl md:text-6xl px-5 text-center">
           expstud.io coming soon
         </h2>
       </motion.div>
-      <motion.div className="absolute left-1/2 transform -translate-x-1/2 bottom-10">
+      <motion.div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-10"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2.5, duration: 1.5, ease: "easeInOut" }}
+      >
         <TwitterIcon />
       </motion.div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         main: "url('/images/background.png')",
         button: "url('/images/button.png')",
         container: "url('/images/container-transparent.png') 30 round",
-        "background-gradient": `linear-gradient(90deg, rgba(20,20,20,1) 60%, rgba(5,5,22,1) 91%);`,
+        // "background-gradient": `linear-gradient(90deg, rgba(20,20,20,1) 60%, rgba(5,5,22,1) 91%);`,
+        "background-gradient": `linear-gradient(0deg, rgba(5,5,22,1) 44%,  rgba(20,20,20,1) 91%)`,
       },
       fontFamily: {
         primary: ["DaysOne"],
