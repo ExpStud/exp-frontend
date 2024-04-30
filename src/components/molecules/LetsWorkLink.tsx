4153c0e2b3defc9e158c77037cf96492b2a694d4
Darth Degen <darthdegen@gmail.com>
2024-04-30 16:16:42 -0700

    feat: added about view respons

--- src/components/molecules/LetsWorkLink.tsx
+++ src/components/molecules/LetsWorkLink.tsx
@@ -1,14 +1,16 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 interface Props {}
 
 const LetsWorkLink: FC<Props> = (props: Props) => {
+  const [hover, setHover] = useState<boolean>(false);
+
   return (
     <div className="left-margin mt-20 mb-20 text-5xl xl:text-8xl">
       <Link href="/contact">
-        <p>Let&apos;s work together.</p>
+        <p className="text-white opacity-80">Let&apos;s work together.</p>
         <Image
           src="/images/arrow.svg"
           alt="arrow"
--- src/components/templates/AboutView.tsx
+++ src/components/templates/AboutView.tsx
@@ -2,7 +2,12 @@ import { motion } from "framer-motion";
 import { Dispatch, SetStateAction, FC, useContext, useState } from "react";
 import Link from "next/link";
 import { ViewContext, slideUp } from "@constants";
-import { TwitterIcon, WelcomeSection, LetsWorkLink, BackgroundImage } from "@components";
+import {
+  TwitterIcon,
+  WelcomeSection,
+  LetsWorkLink,
+  BackgroundImage,
+} from "@components";
 import Image from "next/image";
 
 interface Props {
@@ -14,26 +19,31 @@ const AboutView: FC<Props> = (props: Props) => {
   const { showView } = useContext(ViewContext);
 
   return (
-    <div className="relative w-full h-full items-center justify-center">
-      
+    <div className="relative w-full h-full items-center justify-center pr-5">
       <BackgroundImage />
 
       <div className="relative z-10">
+        <WelcomeSection title1="About us." />
 
-        <WelcomeSection title1="About us." title2="" />
-
-        <div className="ml-10 mt-20 flex justify-between">
-          <div className="text-5xl font-medium">
-            <p className="text-custom-gray mb-3">Our journey.</p>
-            <p>Perfeggcionism freaks.</p>
+        <div className="left-margin mt-20 flex flex-col lg:flex-row justify-between max-w-[1480px]">
+          <div className="font-medium">
+            <p className="text-5xl text-custom-gray mb-3">Our journey.</p>
+            <p className="text-4xl md:text-5xl">Perfeggcionism freaks.</p>
           </div>
-        
-          <div className="w-1/2 mt-10 text-xl">
-            <p>Founded in January 2023, EXP Studio emerged from the nearly decade-long partnership between Miguel Corzo and Wallace Palmer. Having spent over ten years working together, our collaborative journey began in diverse industries. Transitioning from SaaS engineers to trailblazers in the web 3 space, we bring authentic expertise to the table.</p>
+
+          <div className="lg:w-1/2 mt-10 text-xl max-w-[608px]">
+            <p>
+              Founded in January 2023, EXP Studio emerged from the nearly
+              decade-long partnership between Miguel Corzo and Wallace Palmer.
+              Having spent over ten years working together, our collaborative
+              journey began in diverse industries. Transitioning from SaaS
+              engineers to trailblazers in the web 3 space, we bring authentic
+              expertise to the table.
+            </p>
           </div>
         </div>
 
-        <div className="ml-10 mt-20 flex flex-row text-xl text-custom-gray">
+        <div className="left-margin mt-20 flex flex-row text-xl text-custom-gray">
           <div>
             <p className="text-9xl font-light text-custom-purple">03</p>
             <p>Years active</p>
@@ -47,55 +57,80 @@ const AboutView: FC<Props> = (props: Props) => {
 
         <hr className="border-gray-700 mt-20"></hr>
 
-        <div className="ml-10 mt-20 text-5xl font-medium">
-          <p className="text-custom-gray mb-3">Our team.</p>
-          <p>Perfeggcionism freaks.</p>
+        <div className="left-margin mt-20 text-5xl font-medium">
+          <p className="text-5xl text-custom-gray mb-3">Our team.</p>
+          <p className="text-4xl md:text-5xl">Perfeggcionism freaks.</p>
 
-          <p className="text-xl w-1/2 mt-10 font-normal">What sets us apart is not just our services but the passion and expertise each team member brings. We thrive on challenges, push creative boundaries, and are dedicated to delivering solutions that exceed expectations.</p>
+          <p className="text-xl lg:w-1/2 mt-10 font-normal">
+            What sets us apart is not just our services but the passion and
+            expertise each team member brings. We thrive on challenges, push
+            creative boundaries, and are dedicated to delivering solutions that
+            exceed expectations.
+          </p>
         </div>
 
-        <div className="ml-10 mt-20 flex">
-
+        <div className="left-margin mt-20 flex flex-col lg:flex-row gap-2">
           <Image
-              src="/images/about/14-nacho.jpg"
-              alt="Miguel"
-              width={608}
-              height={608}
-              className="grayscale rounded-md"
-            />
+            src="/images/about/14-nacho.jpg"
+            alt="Miguel"
+            width={608}
+            height={608}
+            className="rounded-md grayscale"
+          />
 
-          <div className="flex flex-col justify-items-start w-1/2 ml-10">
-            <p>Miguel Corzo</p>
-            <p className="text-custom-gray">Project Manager and Designer: With a decade-long career, Miguel brings a wealth of experience to the table. As EXP&apos;s project manager and designer, Miguel orchestrates seamless collaborations, and development cycles ensuring your vision comes to life with precision and creativity.</p>
+          <div className="flex flex-col justify-items-start lg:w-1/2 max-w-[608px] lg:ml-10">
+            <p>Miguel Corzo (Otablem)</p>
+            <p className="text-custom-gray">
+              With a decade-long career, Miguel brings a wealth of experience to
+              the table. As EXP&apos;s project manager and founder, Miguel
+              orchestrates seamless collaborations, and development cycles
+              ensuring your vision comes to life with precision and creativity.
+            </p>
 
-            <p className="mt-5">Experience</p>
+            <p className="mt-5">Web 3 Experience</p>
             <div className="text-custom-gray">
               <p>
-                My Slimes & All in Time (1 year and counting)
+                My Slimes & All in Time Project Manager (2 years and counting)
               </p>
-              <p>
-                Project Manager at DeGods & y00ts (1 year)
-              </p>
-              <p>
-                Hot Heads (1 year)
-              </p>
+              <p>Slimes Studio Project Manager (1 year and counting)</p>
+              <p>Project Manager at DeGods & y00ts (1 year) </p>
+              <p>Project Manager at Hot Heads (1 year)</p>
+            </div>
+
+            <p className="mt-5">Web 2 Experience</p>
+            <div className="text-custom-gray">
+              <p>Account Support & Customer Service (1 year)</p>
+              <p>Technical Support (1 year)</p>
+              <p>Mobile Application Specialist (3 years)</p>
+              <p>Product Owner & Manager (1 year)</p>
+              <p>Software Sales (1 year) </p>
             </div>
           </div>
         </div>
 
-        <div className="ml-10 flex items-end">
-          <div className="flex flex-col w-1/2">
+        <div className="left-margin mt-10 flex flex-col-reverse lg:flex-row items-end gap-2">
+          <div className="flex flex-col lg:w-1/2 max-w-[608px]">
             <p>Wallace Palmer</p>
-            <p className="text-custom-gray">As EXP&apos;s lead developer, Wallace is the driving force behind the technical brilliance at EXP Studio. With over 10 years development experience in various industries he has evolved from SaaS web & mobile engineering to pioneering modern solutions on the Solana Blockchain.</p>
+            <p className="text-custom-gray">
+              As EXP&apos;s lead developer, Wallace is the driving force behind
+              the technical brilliance at EXP Studio. With over 10 years
+              development experience in various industries he has evolved from
+              SaaS web & mobile engineering to pioneering modern solutions on
+              the Solana Blockchain.
+            </p>
 
-            <p className="mt-5">Experience</p>
+            <p className="mt-5">Web 3 Experience</p>
             <div className="text-custom-gray">
-              <p>
-                Liberty Square (1 year and counting)
-              </p>
-              <p>
-                Lead Developer at DeGods & y00ts (1 year)
-              </p>
+              <p>Lead Developer at DeGods & y00ts(1 year)</p>
+              <p>Liberty Square (1 year)</p>
+              <p>EXP (2 years)</p>
+            </div>
+
+            <p className="mt-5">Web 2 Experience</p>
+            <div className="text-custom-gray">
+              <p>Mobile Application Development Lead (5 year)</p>
+              <p>Full Stack Web Developer (2 years)</p>
+              <p>Technical Support (2 years)</p>
             </div>
           </div>
 
@@ -104,16 +139,14 @@ const AboutView: FC<Props> = (props: Props) => {
             alt="Wallace"
             width={608}
             height={608}
-            className="grayscale rounded-md"
+            className="rounded-md grayscale"
           />
         </div>
 
         <hr className="border-gray-700 mt-20"></hr>
 
         <LetsWorkLink />
-
       </div>
-
     </div>
   );
 };
