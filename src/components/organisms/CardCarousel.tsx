31752100cd519b76517484107c612bbe4abb8a75
Darth Degen <darthdegen@gmail.com>
2024-05-03 10:00:23 -0700

    feat: added slider

--- src/components/organisms/CardCarousel.tsx
+++ src/components/organisms/CardCarousel.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Carousel, Client, clients } from "@constants";
 import { Button } from "@components";
 import { useRouter } from "next/router";
+import { PanInfo, motion } from "framer-motion";
 
 // interface Card {
 //   name: string;
@@ -17,6 +18,8 @@ const CardCarousel = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
+  const [sliderValue, setSliderValue] = useState(0);
+
   const sliderRef = useRef<HTMLDivElement>(null);
 
   const startDragging = (e: React.MouseEvent | React.TouchEvent) => {
@@ -45,48 +48,112 @@ const CardCarousel = () => {
     const x = clientX - sliderRef.current.offsetLeft;
     const walk = (x - startX) * 3;
     sliderRef.current.scrollLeft = scrollLeft - walk;
+
+    // console.log(sliderRef.current.scrollLeft); // Logs the scroll position
   };
 
   // const navigationWidth = "6.5rem";
 
+  const handleScroll = () => {
+    if (sliderRef.current) {
+      const scrollPosition = sliderRef.current.scrollLeft;
+      const fullWidth = sliderRef.current.scrollWidth;
+      const scrollPercentage = (scrollPosition / fullWidth) * 100;
+      const scrollPercentSlider = scrollPercentage * 0.01 * 145;
+      console.log(scrollPercentSlider); // Logs the scroll position as a percentage of the full width
+      setSliderValue(scrollPercentSlider);
+    }
+  };
+
   return (
-    <div className="my-10 lg:my-20  overflow-x-auto" style={{ width: "100%" }}>
-      <div
-        ref={sliderRef}
-        className="flex cursor-pointer gap-2"
-        onMouseDown={startDragging}
-        onMouseLeave={stopDragging}
-        onMouseUp={stopDragging}
-        onMouseMove={onDrag}
-        onTouchStart={startDragging}
-        onTouchEnd={stopDragging}
-        onTouchMove={onDrag}
-        style={{
-          scrollBehavior: "smooth",
-          scrollbarWidth: "none",
-          msOverflowStyle: "none",
-        }}
-      >
-        <style>
-          {`
+    <div
+      className="my-10 lg:my-20 flex flex-col items-center"
+      // style={{ width: `calc(100vw - ${navigationWidth})` }}
+    >
+      <div className="" style={{ width: "100%" }}>
+        <div
+          ref={sliderRef}
+          className="flex cursor-pointer gap-2 overflow-x-auto"
+          onMouseDown={startDragging}
+          onMouseLeave={stopDragging}
+          onMouseUp={stopDragging}
+          onMouseMove={onDrag}
+          onTouchStart={startDragging}
+          onTouchEnd={stopDragging}
+          onTouchMove={onDrag}
+          onScroll={handleScroll}
+          style={{
+            scrollBehavior: "smooth",
+            scrollbarWidth: "none",
+            msOverflowStyle: "none",
+          }}
+        >
+          <style>
+            {`
               ::-webkit-scrollbar {
                 display: none;
               }
             `}
-        </style>
-        {clients.map((client) => (
-          <>
-            {client.carousel?.map((item, index) => (
-              <CarouselItem
-                key={index}
-                index={index}
-                client={client}
-                carousel={item}
-              />
-            ))}
-          </>
-        ))}
+          </style>
+          {clients.map((client) => (
+            <>
+              {client.carousel?.map((item, index) => (
+                <CarouselItem
+                  key={index}
+                  index={index}
+                  client={client}
+                  carousel={item}
+                />
+              ))}
+            </>
+          ))}
+        </div>
       </div>
+      <CarouselSlider
+        sliderValue={sliderValue}
+        setSliderValue={setSliderValue}
+      />
+    </div>
+  );
+};
+
+interface CarouselSliderProps {
+  sliderValue: number;
+  setSliderValue: (value: number) => void;
+}
+const CarouselSlider: FC<CarouselSliderProps> = (
+  props: CarouselSliderProps
+) => {
+  const { sliderValue, setSliderValue } = props;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // const handleDrag = (e: MouseEvent | TouchEvent, info: PanInfo) => {
+  //   if (containerRef.current) {
+  //     const newSliderValue = Math.max(
+  //       0,
+  //       Math.min(
+  //         info.point.x,
+  //         containerRef.current.offsetWidth - 60 // Subtract the width of the slider
+  //       )
+  //     );
+  //     console.log("newSliderValue ", newSliderValue);
+  //     setSliderValue(newSliderValue);
+  //   }
+  // };
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative flex justify-center gap-2 mt-6 h-[1px] w-[176px] bg-white bg-opacity-20"
+    >
+      <motion.div
+        className="absolute bg-white w-[60px] h-[1px] cursor-pointer"
+        initial={{ left: 0 }}
+        animate={{ left: sliderValue }}
+        // drag="x"
+        // dragConstraints={containerRef}
+        // onDrag={handleDrag}
+      />
     </div>
   );
 };
@@ -108,7 +175,7 @@ const CarouselItem: FC<CarouselItemProps> = (props: CarouselItemProps) => {
 
   return (
     <div
-      className={`relative h-[600px] flex items-end justify-between rounded-lg mr-4 ${
+      className={`relative h-[600px] flex items-end justify-between rounded-lg ${
         carousel.backgroundColor
       } ${index === 0 ? "ml-4 md:ml-10" : "ml-4"}`}
       style={{ minWidth: 1040 }}
