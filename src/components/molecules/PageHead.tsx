13935d9d6724ced0114117fffb11b313d7d3892b
Darth Degen <darthdegen@gmail.com>
2025-05-19 15:29:58 -0700

    updated page head

--- src/components/molecules/PageHead.tsx
+++ src/components/molecules/PageHead.tsx
@@ -23,14 +23,20 @@ const PageHead: FC<Props> = (props: Props) => {
         <meta name="twitter:site" content={`@${twitter}`} />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={`${url}/meta.jpg`} />
+        <meta
+          name="twitter:image"
+          content={`https://images.sandboxstud.io/images/meta.jpg`}
+        />
         <meta property="twitter:url" content={url} />
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={`/meta.jpg`} />
+        <meta
+          property="og:image"
+          content={`https://images.sandboxstud.io/images/meta.jpg`}
+        />
       </Head>
       {/* Google Ads Script */}
       <Script
