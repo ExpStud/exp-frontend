df34ca227f4781a1d32effa14f82471f716f6fcb
Wallace Palmer <darthdegen@gmail.com>
2026-01-15 09:25:51 -0700

    updated team titles

--- src/constants/teamMembers.ts
+++ src/constants/teamMembers.ts
@@ -1,17 +1,16 @@
 import { TeamMember } from "@constants";
 
-
 export const teamMembers: TeamMember[] = [
   {
     name: "Miguel Corzo (Blem)",
-    title: "Founder & Project Lead",
+    title: "Project Lead",
     image: `${process.env.CLOUDFLARE_STORAGE}/images/about/sketch-miguel.png`,
     twitter: "https://x.com/otablem",
     linkedin: "https://www.linkedin.com/in/miguel-andres-corzo",
   },
   {
     name: "Wallace Palmer (Darth)",
-    title: "Founder & Lead Developer",
+    title: "Lead Developer",
     image: `${process.env.CLOUDFLARE_STORAGE}/images/about/sketch-wallace.png`,
     twitter: "https://x.com/DarthDegen",
     linkedin: "https://www.linkedin.com/in/wallace-palmer-4b93473a/",
