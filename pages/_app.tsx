dcff8dfd44f10bd74b99873070ea4441e635a92a
Wallace Palmer <darthdegen@gmail.com>
2026-09-18 05:53:14 -0700

    feat: adding PageMarkets script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 import { AppProvider } from "@contexts";
 import { useEffect } from "react";
 import { Header } from "@components";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
@@ -12,31 +13,39 @@ const App = ({ Component, pageProps }: AppProps) => {
       // your console.log here
       console.log(
         `%cSandbox Studio - built with Next.js, Tailwind, and a lot of late nights 🌙 `,
-        "color: #60a5fa; font-family: monospace; font-size: 12px;"
+        "color: #60a5fa; font-family: monospace; font-size: 12px;",
       );
     }
   }, []);
 
   return (
-    <AppProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            border: "2px solid #181818",
-            padding: "10px",
-            color: "#181818",
-            backgroundColor: "#F0E3AA",
-          },
-          iconTheme: {
-            primary: "#181818",
-            secondary: "#F0E3AA",
-          },
-        }}
+    <>
+      <AppProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              border: "2px solid #181818",
+              padding: "10px",
+              color: "#181818",
+              backgroundColor: "#F0E3AA",
+            },
+            iconTheme: {
+              primary: "#181818",
+              secondary: "#F0E3AA",
+            },
+          }}
+        />
+      </AppProvider>
+      <Script
+        src="https://pagemarkets.com/v1/loader.js"
+        data-site="0x8f20fac83005747594cd2862a9e37aaf6c332960"
+        data-place=""
+        strategy="lazyOnload"
       />
-    </AppProvider>
+    </>
   );
 };
 
